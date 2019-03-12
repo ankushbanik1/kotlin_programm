d4e0ea1b3b8e4ba9f3701ac75aac0e4d7d940131
ankush banik <ankushbanik1234@gmail.com>
2019-03-12 20:02:56 +0530

    today progress bar,web view,page opening count
    today progress bar,web view,page opening count

--- combo/app/src/main/java/com/example/combo/MainActivity.kt
+++ combo/app/src/main/java/com/example/combo/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.combo
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onBackPressed() {
+        val builder= AlertDialog.Builder(this)
+        builder.setTitle("Are You Sure?")
+        builder.setMessage("Do You Want To close Application?")
+        builder.setPositiveButton("Yes",{ dialogInterface: DialogInterface, i: Int ->
+
+            finish()
+        })
+        builder.setNegativeButton("No",{ dialogInterface: DialogInterface, i: Int -> })
+
+   builder.show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
